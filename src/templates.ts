30f9c2e0fa21092bc55f7659458dd54d5834513a
samuelreichor <samuelreichor@gmail.com>
2025-07-14 19:58:41 +0200

    Set up basic CLI structure

--- src/templates.ts
+++ src/templates.ts
@@ -9,25 +9,25 @@ export const templates: Template[] = [
   {
     name: 'Vue',
     value: 'vueJs',
-    repositoryLink: 'https://github.com/samuelreichor/query-api-craft-starter',
+    repositoryLink: 'samuelreichor/query-api-craft-starter',
     branch: 'examples/vue',
   },
   {
     name: 'Nuxt',
     value: 'nuxt',
-    repositoryLink: 'https://github.com/samuelreichor/query-api-craft-starter',
+    repositoryLink: 'samuelreichor/query-api-craft-starter',
     branch: 'examples/nuxt',
   },
   {
     name: 'React Vite',
     value: 'reactJs',
-    repositoryLink: 'https://github.com/samuelreichor/query-api-craft-starter',
+    repositoryLink: 'samuelreichor/query-api-craft-starter',
     branch: 'examples/react-vite',
   },
   {
     name: 'Next.js',
     value: 'nextJs',
-    repositoryLink: 'https://github.com/samuelreichor/query-api-craft-starter',
+    repositoryLink: 'samuelreichor/query-api-craft-starter',
     branch: 'examples/next',
   },
 ]
